b3c876f843e2c5ae68d04409c104ca788ac3c08c
pmint93 <phamminhthanh69@gmail.com>
2020-07-14 15:11:48 +0700

    Setting CORS when running with gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,20 @@
 import os
 import logging
+from flask_cors import CORS
 from app import blueprint
 from app.main import create_app
 
 app = create_app(os.getenv('APPLICATION_ENV') or 'development')
+if app.config.get('ENABLE_CORS', False):
+    CORS(
+        blueprint,
+        origins=app.config.get('CORS_ALLOW_ORIGINS') or '*',
+        methods=app.config.get('CORS_ALLOW_METHODS') or ['GET', 'HEAD', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+        allow_headers=app.config.get('CORS_ALLOW_HEADERS') or '*',
+        expose_headers=app.config.get('CORS_EXPOSE_HEADERS'),
+        supports_credentials=bool(app.config.get('CORS_ALLOW_CREDENTIALS', False)),
+        max_age=app.config.get('CORS_MAX_AGE')
+    )
 app.register_blueprint(blueprint)
 gunicorn_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers = gunicorn_logger.handlers
